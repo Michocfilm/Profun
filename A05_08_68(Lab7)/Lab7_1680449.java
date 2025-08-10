ebf47cae93bed2c54d9685dd7fed990a2827ee6c
Michocfilm <104713168+Michocfilm@users.noreply.github.com>
2025-08-10 12:02:59 +0700

    up

--- A05_08_68(Lab7)/Lab7_1680449.java
+++ A05_08_68(Lab7)/Lab7_1680449.java
@@ -3,15 +3,13 @@ public class Lab7_1680449{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Input : ");
-        String input = sc.nextLine();
-        String[] token = input.split(" ");
-        System.out.println("Output : "+q2_1_myIndexOF(token[0], token[1]));
+        String[] input = sc.nextLine().split(" ");
+        System.out.println("Output : "+q2_1_myIndexOF(input[0], input[1]));
         sc.close();
     }
     static int q2_1_myIndexOF(String str,String sub){
         for (int i = 0; i <= str.length() - sub.length(); i++) {
             boolean match = true;
-            // ตรวจทีละตัวอักษรใน sub
             for (int j = 0; j < sub.length(); j++) {
                 if (str.charAt(i + j) != sub.charAt(j)) {
                     match = false;
--- A05_08_68(Lab7)/Lab7_2680449.java
+++ A05_08_68(Lab7)/Lab7_2680449.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 public class Lab7_2680449 {
     public static void main(String[] args){
-
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Input : ");
+        String[] input = sc.nextLine().split(" ");
+        System.out.println(q2_2_containsAndBefore(input[0], input[1], input[2]));
     }
     static boolean q2_2_containsAndBefore(String input,String sub1,String sub2){
-        
+        for(int i=0;i<input.length();i++){
+            
+        }
+        return true;
     }
 }
